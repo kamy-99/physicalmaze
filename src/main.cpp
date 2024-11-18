db002b9c959964f788b265320084d96527d63491
kamy-99 <kamyab.rayganshirazinejad@student.nhlstenden.com>
2024-11-18 09:18:43 +0100

    movement

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,11 @@
 #define Motor_B1 9  //9
 #define Motor_B2 3  //3
 
-void rotate_l(int speed);
+void rotate_l();
+void rotate_r();
 void forward(int speed);
 void stop_();
 void backwards(int speed);
-void rotate_r(int speed);
 void turn_l(int speed);
 void turn_r(int speed);
 
@@ -23,25 +23,35 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  // forward(255);
-  // delay(2000);
-  // backwards(255);
-  // delay(2000);
-  // rotate_r(200);
-  // delay(1000);
-  // rotate_l(200);
-  // delay(1000);
-  // turn_l(200);
-  // delay(1500);
-  // turn_r(200);
-  // delay(2000);
-  // stop_();
-  // delay(5000);
+   forward(255);
+   delay(3000);
+   stop_();
+   delay(3000);
+
+   backwards(255);
+   delay(3000);
+   stop_();
+   delay(3000);
+
+   turn_l(255);
+   stop_();
+   delay(3000);
+
+   turn_r(255);
+   stop_();
+   delay(3000);
+
+   rotate_r();
+   stop_();
+   delay(3000);
+  
+   rotate_l();
+   stop_();
+   delay(3000);
 }
 
 void forward(int speed) {
-  analogWrite(Motor_A1, 0);
-  analogWrite(Motor_B1, 0);
+  stop_();
   analogWrite(Motor_A2, 255);
   analogWrite(Motor_B2, 255);
   delay(10);
@@ -56,39 +66,56 @@ void stop_() {
 }
 
 void backwards(int speed) { // done
-  analogWrite(Motor_A2, 0);
-  analogWrite(Motor_B2, 0);
+  stop_();
   analogWrite(Motor_A1, 255);
   analogWrite(Motor_B1, 255);
   delay(10);
-  analogWrite(Motor_A1, speed * 0.95);
+  analogWrite(Motor_A1, speed * 0.98);
   analogWrite(Motor_B1, speed);
 }
 
 void turn_r(int speed) {
-  analogWrite(Motor_A1, 0);
-  analogWrite(Motor_B1, 0);
+  stop_();
+  analogWrite(Motor_A2, 255);
+  analogWrite(Motor_B2, 255);
+  delay(10);
   analogWrite(Motor_A2, speed);
-  analogWrite(Motor_B2, speed * 0.5);
+  analogWrite(Motor_B2, speed * 0.8);
+  delay(8000);
 }
 
 void turn_l(int speed) {
-  analogWrite(Motor_A1, 0);
-  analogWrite(Motor_B1, 0);
-  analogWrite(Motor_A2, speed*0.5);
+  stop_();
+  analogWrite(Motor_A2, 255);
+  analogWrite(Motor_B2, 255);
+  delay(10);
+  analogWrite(Motor_A2, speed*0.7);
   analogWrite(Motor_B2, speed);
+  delay(4000);
 }
 
-void rotate_r(int speed) {
-  analogWrite(Motor_A1, 0);
+void rotate_r() {
+  stop_();
+  for (int i = 0; i < 55; i++) {
+    analogWrite(Motor_B1, 170);
+    analogWrite(Motor_A2, 255);
+    delay(10);
+    analogWrite(Motor_A2, 170);
+    delay(1);
+  }
   analogWrite(Motor_B2, 0);
-  analogWrite(Motor_A2, speed);
-  analogWrite(Motor_B1, speed);
+  analogWrite(Motor_A1, 0);
 }
 
-void rotate_l(int speed) {
-  analogWrite(Motor_A2, 0);
-  analogWrite(Motor_B1, 0);
-  analogWrite(Motor_A1, speed);
-  analogWrite(Motor_B2, speed);
+void rotate_l() {
+  stop_();
+  for (int i = 0; i < 55; i++) {
+    analogWrite(Motor_B2, 170);
+    analogWrite(Motor_A1, 255);
+    delay(10);
+    analogWrite(Motor_A1, 170);
+    delay(1);
+  }
+  analogWrite(Motor_B2, 0);
+  analogWrite(Motor_A1, 0);
 }
