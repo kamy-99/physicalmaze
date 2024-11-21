51f15cd819b6a0a0ef4edf2b16b0ee9d7150d7e5
neszm <neszmark@gmail.com>
2024-11-21 10:52:27 +0100

    did rotate_r, backwards, forwards, distance in cm

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,15 @@
 #define MOTOR_B2 5  //5
 #define ROT_R1 2 // R
 #define ROT_R2 3 // L
-#define DVALUE 10 // debounce value
+#define DVALUE 10 // debounce value in ms
+#define PI 3.141592653589793238462643383279502884197 // 39 digits
 
-void rotate_l(); // because of platform IO
+// because of platform IO
+void rotate_l();
 void rotate_r();
-void forward(int speed);
+void forward(int distance, int speed);
+void backwards(int distance, int speed);
 void stop_();
-void backwards(int speed);
 void turn_l(int speed);
 void turn_r(int speed);
 void updaterotation_R2();
@@ -32,19 +34,29 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(ROT_R2), updaterotation_R2, CHANGE);
 }
 
+int WheelC = 6.5*PI; // wheel circumference in cm 2*r*pi ~20.5cm 
+
 void loop() {
   rotate_l();
   delay(5000);
 }
 
-void forward(int speed) {
+void forward(int distance, int speed) { // distance in cm & speed
   stop_();
+  int rotations = distance / WheelC;
+  RRotation = 0;
+  LRotation = 0;
   analogWrite(MOTOR_A2, 255);
   analogWrite(MOTOR_B2, 255);
-  delay(10);
-  analogWrite(MOTOR_A2, speed);
-  analogWrite(MOTOR_B2, speed);
+  delay(5);
+  while (RRotation <= rotations && LRotation <= rotations) {
+    analogWrite(MOTOR_A2, speed);
+    analogWrite(MOTOR_B2, speed);
+  }
+  RRotation = 0;
+  LRotation = 0;
 }
+
 void stop_() {
   analogWrite(MOTOR_A2, 0);
   analogWrite(MOTOR_B2, 0);
@@ -52,16 +64,23 @@ void stop_() {
   analogWrite(MOTOR_B1, 0);
 }
 
-void backwards(int speed) { // done
+void backwards(int distance, int speed) { // distance in cm & speed
   stop_();
+  int rotations = distance / WheelC;
+  RRotation = 0;
+  LRotation = 0;
   analogWrite(MOTOR_A1, 255);
   analogWrite(MOTOR_B1, 255);
-  delay(10);
-  analogWrite(MOTOR_A1, speed * 0.98);
-  analogWrite(MOTOR_B1, speed);
+  delay(5);
+  while (RRotation <= rotations && LRotation <= rotations) {
+    analogWrite(MOTOR_A1, speed * 0.98);
+    analogWrite(MOTOR_B1, speed);
+  }
+  RRotation = 0;
+  LRotation = 0;
 }
 
-void turn_r(int speed) {
+void turn_r(int speed) { // don't need to change because we are not using turn only rotate
   stop_();
   analogWrite(MOTOR_A2, 255);
   analogWrite(MOTOR_B2, 255);
@@ -71,7 +90,7 @@ void turn_r(int speed) {
   delay(8000);
 }
 
-void turn_l(int speed) {
+void turn_l(int speed) { // don't need to change because we are not using turn only rotate
   stop_();
   analogWrite(MOTOR_A2, 255);
   analogWrite(MOTOR_B2, 255);
@@ -83,15 +102,16 @@ void turn_l(int speed) {
 
 void rotate_r() {
   stop_();
-  for (int i = 0; i < 55; i++) {
-    analogWrite(MOTOR_B1, 170);
-    analogWrite(MOTOR_A2, 255);
-    delay(10);
+  analogWrite(MOTOR_A2, 255);
+  analogWrite(MOTOR_B1, 255);
+  delay(5);
+  while (RRotation <= 17 && LRotation <= 17) { // 17 is roughly 90 degrees w/ this speed but if we want to make it faster we have to adjust
     analogWrite(MOTOR_A2, 170);
-    delay(1);
+    analogWrite(MOTOR_B1, 170);
   }
-  analogWrite(MOTOR_B2, 0);
-  analogWrite(MOTOR_A1, 0);
+  RRotation = 0;
+  LRotation = 0;
+  stop_();
 }
 
 void rotate_l() {
@@ -100,7 +120,7 @@ void rotate_l() {
   analogWrite(MOTOR_A1, 255);
   analogWrite(MOTOR_B2, 255);
   delay(5);
-  while (RRotation <= 18 && LRotation <= 18) {
+  while (RRotation <= 17 && LRotation <= 17) { // 17 is roughly 90 degrees w/ this speed but if we want to make it faster we have to adjust
     analogWrite(MOTOR_A1, 170);
     analogWrite(MOTOR_B2, 170);
   }
