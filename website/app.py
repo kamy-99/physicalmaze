775e3c60d0b13f3891923a4f50b399c5511525ee
neszm <neszmark@gmail.com>
2025-01-14 14:33:39 +0100

    should show speed now (rly bad but its visual)

--- website/app.py
+++ website/app.py
@@ -4,35 +4,55 @@ from flask import Flask, render_template, Response, request, jsonify, stream_wit
 import time
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 # this function is for receiveing the data from the master
-@app.route('/api/data', methods=['POST'])
+@app.route('/api/data', methods=['POST', "GET"])
 def receive_data():
-    print("Received headers:", request.headers)
-    print("Received data:", request.get_data())
-    if not request.is_json: # make sure it's json
-        return jsonify({"error": "Request must be JSON"}), 400
+    if request.method == "POST":
+        print("Received headers:", request.headers)
+        print("Received data:", request.get_data())
+        if not request.is_json: # make sure it's json
+            return jsonify({"error": "Request must be JSON"}), 400
 
-    data = request.get_json()
-    print("data from arduino:", data, flush=True)
-    # had to make them short because it takes too much time to transmit full names like "slave_num" then just a number etc
-    slave_num = data.get('sn')
-    speed = data.get('s')
-    lrotation = data.get('lr')
-    rrotation = data.get('rr')
-    sonar = data.get('so')
-    action = data.get('a')
+        data = request.get_json()
+        print("data from arduino:", data, flush=True)
+        # had to make them short because it takes too much time to transmit full names like "slave_num" then just a number etc
+        slave_num = data.get('sn')
+        speed = data.get('s')
+        lrotation = data.get('lr')
+        rrotation = data.get('rr')
+        sonar = data.get('so')
+        action = data.get('a')
 
-    if not all([slave_num, speed, lrotation, rrotation, sonar, action]): # if not all data are here return an error
-        return jsonify({"error": "Missing required fields"}), 400
+        if not all([slave_num, speed, lrotation, rrotation, sonar, action]): # if not all data are here return an error
+            return jsonify({"error": "Missing required fields"}), 400
 
-    try:
-        insert_into_db(slave_num, speed, lrotation, rrotation, sonar, action) # just try put them into the DB
-    except sqlite3.Error as e:
-        print(f"Database insert failed: {e}")
+        try:
+            insert_into_db(slave_num, speed, lrotation, rrotation, sonar, action) # just try put them into the DB
+        except sqlite3.Error as e:
+            print(f"Database insert failed: {e}")
+        
+        return jsonify({"message": "Data received successfully"}), 200
+    elif request.method == "GET":
+        conn = get_db_conn()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM robot ORDER BY created_at DESC LIMIT 1")
+        result = cursor.fetchone()
+
+        cursor.close()
+        if result:
+            return jsonify({'slave_num': data['slave_num'],
+                    'speed': data['speed'],
+                    'lrotation': data['lrotation'],
+                    'rrotation': data['rrotation'],
+                    'sonar': data['sonar'],
+                    'action': data['action'],
+                    'time': data['created_at']}), 200
+
+        # Default response if no data is found
+        return jsonify({'speed': 150}), 200
     
-    return jsonify({"message": "Data received successfully"}), 200
-
 # this is the function that is important for the real time data
 @app.route('/stream')
 def stream():
@@ -101,6 +121,10 @@ init_db()
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 @app.route("/linefollower")
 def line():
